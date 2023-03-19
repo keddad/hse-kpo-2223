97177706b3f7012a418ab62a481310e1be1cf7d8
keddad <keddad@yandex.ru>
2023-03-20 00:19:31 +0300

    Stasi: .json simplification

--- stasi/src/main/java/edu/keddad/stasi/Manager/MenuDishes.java
+++ stasi/src/main/java/edu/keddad/stasi/Manager/MenuDishes.java
@@ -11,19 +11,13 @@ public class MenuDishes {
     @AllArgsConstructor
     public static class MenuDish {
 
-        @JsonProperty("menu_dish_id")
         public int id;
 
-        @JsonProperty("menu_dish_card")
-        public int card;
-
-        @JsonProperty("menu_dish_price")
         public int price;
 
-        @JsonProperty("menu_dish_active")
+        @JsonProperty("is_active")
         public boolean active;
     }
 
-    @JsonProperty("menu_dishes")
     public MenuDish[] dishes;
 }
