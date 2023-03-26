22ffd2ac36c855103e213863479802425519f0ca
bogdanlukancuk <ibogdan533@gmail.com>
2023-03-26 22:22:25 +0300

    Client is ready?

--- stasi/src/main/java/edu/keddad/stasi/Client/Client.java
+++ stasi/src/main/java/edu/keddad/stasi/Client/Client.java
@@ -2,6 +2,8 @@ package edu.keddad.stasi.Client;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.keddad.stasi.EqipmentAgent.MenuEqipment;
+import edu.keddad.stasi.InstructionStorage.InstructionAnswer;
 import edu.keddad.stasi.Manager.MenuRequest;
 import edu.keddad.stasi.Manager.MenuResponse;
 import edu.keddad.stasi.Manager.OrderRequest;
@@ -16,71 +18,75 @@ import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
 import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.UUID;
 
 public class Client extends Agent {
+    private ClientPull client;
+
     @Override
     protected void setup() {
 
         DFAgentDescription manager = YellowBooks.findRecipient(this, "manager");
+        try {
+            client = new ObjectMapper().readValue(Paths.get((String) getArguments()[1]).toFile(), ClientPull.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        addBehaviour(new TickerBehaviour(this, 15000) {
-            @Override
-            protected void onTick() {
-                OrderRequest rq = new OrderRequest(new OrderEntry[]{new OrderEntry(1, 2), new OrderEntry(2, 2), new OrderEntry(3, 4),});
+        OrderRequest rq = new OrderRequest(client.ord_dishes);
+        try {
+            String message = OrderRequest.mnemonic + " " + new ObjectMapper().writeValueAsString(rq);
 
-                try {
-                    String message = OrderRequest.mnemonic + " " + new ObjectMapper().writeValueAsString(rq);
+            ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
 
-                    ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+            msg.addReceiver(manager.getName());
+            msg.setContent(message);
+            msg.setReplyWith(UUID.randomUUID().toString());
+            msg.setInReplyTo(""); // workaround for message filtering
 
-                    msg.addReceiver(manager.getName());
-                    msg.setContent(message);
-                    msg.setReplyWith(UUID.randomUUID().toString());
-                    msg.setInReplyTo(""); // workaround for message filtering
+            send(msg);
 
-                    send(msg);
+            System.out.println("Sent a new order to Manager!");
 
-                    System.out.println("Sent a new order to Manager!");
+            addBehaviour(new Behaviour() {
 
-                    myAgent.addBehaviour(new Behaviour() {
+                boolean processed = false;
 
-                        boolean processed = false;
+                @Override
+                public void action() {
+                    MessageTemplate mt = MessageTemplate.MatchInReplyTo(msg.getReplyWith());
 
-                        @Override
-                        public void action() {
-                            MessageTemplate mt = MessageTemplate.MatchInReplyTo(msg.getReplyWith());
+                    ACLMessage reply = myAgent.receive(mt);
+                    ;
 
-                            ACLMessage reply = myAgent.receive(mt);
-                            ;
-
-                            if (reply != null) {
-                                if (reply.getPerformative() == ACLMessage.CONFIRM) {
-                                    System.out.println("We made an order!");
-                                } else if (reply.getPerformative() == ACLMessage.FAILURE) {
-                                    System.out.println("No order was placed.");
-                                } else {
-                                    throw new RuntimeException("Broken invariant");
-                                }
-
-                                processed = true;
-                            } else {
-                                block();
-                            }
+                    if (reply != null) {
+                        if (reply.getPerformative() == ACLMessage.CONFIRM) {
+                            System.out.println("We made an order!");
+                        } else if (reply.getPerformative() == ACLMessage.FAILURE) {
+                            System.out.println("No order was placed.");
+                        } else {
+                            throw new RuntimeException("Broken invariant");
                         }
 
-                        @Override
-                        public boolean done() {
-                            return processed;
-                        }
-                    });
-
-                } catch (JsonProcessingException e) {
-                    throw new RuntimeException(e);
+                        processed = true;
+                    } else {
+                        block();
+                    }
                 }
-            }
-        });
+
+                @Override
+                public boolean done() {
+                    return processed;
+                }
+            });
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
 
         // We ask for menu to demonstrate that we can.
         // Yes, we can.
@@ -113,7 +119,9 @@ public class Client extends Agent {
                         if (reply != null) {
                             try {
                                 MenuResponse mrp = new ObjectMapper().readValue(reply.getContent(), MenuResponse.class);
-                                System.out.println("Currently have this menu items available: " + Arrays.stream(mrp.items).map(it -> {return ((Integer) it.id).toString();}).toList());
+                                System.out.println("Currently have this menu items available: " + Arrays.stream(mrp.items).map(it -> {
+                                    return ((Integer) it.id).toString();
+                                }).toList());
                             } catch (JsonProcessingException e) {
                                 throw new RuntimeException(e);
                             }
---
+++ stasi/src/main/java/edu/keddad/stasi/Client/ClientPull.java
@@ -0,0 +1,14 @@
+package edu.keddad.stasi.Client;
+
+import edu.keddad.stasi.Messaging.OrderEntry;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientPull {
+
+    public long wait_time;
+
+    OrderEntry[] ord_dishes;
+}
